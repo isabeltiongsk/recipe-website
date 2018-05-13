794ccd0e2376f5bebbc44dd3834974b799687f99
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-05-14 00:05:26 +0800

    Update uploadImage.php
    Check if upload button is click and then execute the upload image code

--- uploadImage.php
+++ uploadImage.php
@@ -4,10 +4,11 @@ $title = "Upload new image";
 $content = '<form action="" method="post" enctype="multipart/form-data">
     <label for="file">Filename: </label>
     <input type="file" name="file" id="file"></br>
-    <input type="submit" name="submit" value="submit">
+    <input type="submit" name="upload" value="upload">
 </form>';
 
 // check file type is valid
+if(isset($_POST["upload"])){
 $fileType = $_FILES["file"]["type"];
 
 if(($fileType == "image/gif") ||
@@ -25,6 +26,7 @@ if(($fileType == "image/gif") ||
         move_uploaded_file($_FILES["file"]["tmp_name"], "Images/Recipe/" . $_FILES["file"]["name"]);
         echo "Uploaded in" . "Images/Recipe/" . $_FILES["file"]["name"];
     }
+}
 }
     
 include './Templates.php';
