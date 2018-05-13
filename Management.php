2ff2fb7ad212fd6d0b556b1163d0dac1affffffa
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-05-14 00:01:04 +0800

    Update Management.php
    link to uploadImage.php

--- Management.php
+++ Management.php
@@ -3,8 +3,8 @@ $title = "Management";
 
 $content ='<h3>Recipe</h3>'
         . '<a href="RecipeAdd.php">Add new recipe</a><br/>'
-        . '<a href="">Upload Image</a><br/>'
-        . '<a href="">Recipe Overview</a><br/>';
+        . '<a href="uploadImage.php">Upload Image</a><br/>'
+        . '<a href="RecipeOverview.php">Recipe Overview</a><br/>';
 
 include './Templates.php'
 ?>
