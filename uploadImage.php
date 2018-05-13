e12379b5167515f246f0df8b1e133d7d6242051d
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-05-13 23:59:40 +0800

    Add files via upload
    upload images function
    -check files type
    -check if file is added
    -upload file

---
+++ uploadImage.php
@@ -0,0 +1,32 @@
+<?php
+$title = "Upload new image";
+
+$content = '<form action="" method="post" enctype="multipart/form-data">
+    <label for="file">Filename: </label>
+    <input type="file" name="file" id="file"></br>
+    <input type="submit" name="submit" value="submit">
+</form>';
+
+// check file type is valid
+$fileType = $_FILES["file"]["type"];
+
+if(($fileType == "image/gif") ||
+    ($fileType == "image/jpg") ||
+    ($fileType == "image/jpeg") ||
+    ($fileType == "image/png")) 
+{
+    //check if file exists
+    if(file_exists("Image/Recipe/" . $_FILES["file"]["name"]))
+    {
+        echo "File already exists";
+    }
+    else
+    {
+        move_uploaded_file($_FILES["file"]["tmp_name"], "Images/Recipe/" . $_FILES["file"]["name"]);
+        echo "Uploaded in" . "Images/Recipe/" . $_FILES["file"]["name"];
+    }
+}
+    
+include './Templates.php';
+?>
+
