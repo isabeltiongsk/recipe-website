3bcb61ef7037df89303646c56deedb679a4f78dc
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-05-13 23:45:56 +0800

    Update RecipeAdd.php
    add update function

--- RecipeAdd.php
+++ RecipeAdd.php
@@ -4,7 +4,44 @@ $recipeController = new RecipeController();
 
 $title = "Add new recipe";
 
-$content ="<form action='' method='post'>
+if(isset($_GET["update"]))
+{
+    $recipe = $recipeController->GetRecipeById($_GET["update"]);
+    
+    $content ="<form action='' method='post'>
+    <fieldset>
+        <legend>Add a new recipe</legend>
+        <label for='name'>Name: </label>
+        <input type='text' class='inputField' name='txtName' value='$recipe->name'/><br/>
+        
+        <label for='type'>Type: </label>
+        <select class='inputField' name='ddlType'>
+            <option value='%'>All</option>"
+        .$recipeController->CreateOptionValues($recipeController->GetRecipeType()).
+        
+        "</select><br/>
+        
+        <label for='price'>Price: </label>
+        <input type='text' class='inputField' name='txtPrice' value='$recipe->price'/><br/>
+        
+        <label for='country'>Country: </label>
+        <input type='text' class='inputField' name='txtCountry' value='$recipe->country'/><br/>
+        
+        <label for='image'>Image: </label>
+        <select class='inputField'name='ddlImage'>"
+        .$recipeController->GetImages().
+        "</select></br>
+        
+        <label for='review'>Review: </label>
+        <textarea cols='70' rows='12' name='txtReview'>$recipe->review</textarea></br>
+        
+        <input type='submit'name='submit1' value='Submit'>
+        
+    </fieldset>
+</form>";
+}
+ else {
+    $content ="<form action='' method='post'>
     <fieldset>
         <legend>Add a new recipe</legend>
         <label for='name'>Name: </label>
@@ -35,11 +72,26 @@ $content ="<form action='' method='post'>
         
     </fieldset>
 </form>";
+}
 
-if(isset($_POST["txtName"]))
+
+if(isset($_GET["update"]))
+{
+    if(isset($_POST["txtName"]))
+{
+    $recipeController->UpdateRecipe($_GET["update"]);
+}
+}
+else
+{
+    if(isset($_POST["txtName"]))
 {
     $recipeController->InsertRecipe();
 }
+
+}
+
+
 
 include './Templates.php';
 ?>
