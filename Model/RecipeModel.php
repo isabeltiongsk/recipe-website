dcf511af8d51022335be4a4dfce9afd460754376
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-05-26 22:14:46 +0800

    Update RecipeModel.php

--- Model/RecipeModel.php
+++ Model/RecipeModel.php
@@ -45,9 +45,11 @@ class RecipeModel {
             $country = $row[4];
             $image = $row[5];
             $review = $row[6];
+            $ingredient = $row[7];
+            $instruction = $row[8];
 
             //Create recipe objects and store them in an array.
-            $recipe = new RecipeEntity($id, $name, $type, $price, $country, $image, $review);
+            $recipe = new RecipeEntity($id, $name, $type, $price, $country, $image, $review, $ingredient, $instruction);
             array_push($recipeArray, $recipe);
         }
         //Close connection and return result
@@ -72,9 +74,11 @@ class RecipeModel {
             $country = $row[4];
             $image = $row[5];
             $review = $row[6];
+            $ingredient = $row[7];
+            $instruction = $row[8];
 
             //Create recipe
-            $recipe = new RecipeEntity($id, $name, $type, $price, $country, $image, $review);
+            $recipe = new RecipeEntity($id, $name, $type, $price, $country, $image, $review, $ingredient, $instruction);
         }
         //Close connection and return result
         mysql_close();
@@ -86,31 +90,38 @@ class RecipeModel {
         mysqli_select_db($link, "recipedb");
        
          $query = sprintf("INSERT INTO recipe
-                          (name, type, price,country,image,review)
+                          (name, type, price,country,image,review,ingredient,instruction)
                           VALUES
-                          ('%s','%s','%s','%s','%s','%s')",
+                          ('%s','%s','%s','%s','%s','%s','%s','%s')",
                 $name= mysqli_real_escape_string($link, $recipe->name),
                 $type= mysqli_real_escape_string($link, $recipe->type),
                 $price= mysqli_real_escape_string($link, $recipe->price),
                 $country= mysqli_real_escape_string($link, $recipe->country),
                 $image= mysqli_real_escape_string($link, "Images/Recipe/" . $recipe->image),
-                $review= mysqli_real_escape_string($link, $recipe->review));
+                $review= mysqli_real_escape_string($link, $recipe->review),
+                $ingredient= mysqli_real_escape_string($link, $recipe->ingredient),
+                $instruction= mysqli_real_escape_string($link, $recipe->instruction));
         $this->PerformQuery($query);
     }
         
        
 
     function UpdateRecipe($id, RecipeEntity $recipe) {
+        $link=mysqli_connect("localhost","root","");
+        mysqli_select_db($link, "recipedb");
+       
         $query = sprintf("UPDATE recipe
                             SET name = '%s', type = '%s', price = '%s',
-                            country = '%s', image = '%s', review = '%s'
+                            country = '%s', image = '%s', review = '%s', ingredient = '%s', instruction = '%s'
                           WHERE id = $id",
-                mysqli_real_escape_string($recipe->name),
-                mysqli_real_escape_string($recipe->type),
-                mysqli_real_escape_string($recipe->price),
-                mysqli_real_escape_string($recipe->country),
-                mysqli_real_escape_string("Images/Recipe/" . $recipe->image),
-                mysqli_real_escape_string($recipe->review));
+                mysqli_real_escape_string($link, $recipe->name),
+                mysqli_real_escape_string($link, $recipe->type),
+                mysqli_real_escape_string($link, $recipe->price),
+                mysqli_real_escape_string($link, $recipe->country),
+                mysqli_real_escape_string($link, "Images/Recipe/" . $recipe->image),
+                mysqli_real_escape_string($link, $recipe->review),
+                mysqli_real_escape_string($link, $recipe->ingredient),
+                mysqli_real_escape_string($link, $recipe->instruction));
                           
         $this->PerformQuery($query);
     }
