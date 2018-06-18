9a4bb020079f8e24c48c6e43f9c7bb47720ffec3
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-06-18 13:10:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,30 @@
 <?php
 $title = "Home";
-$content = '<img src="Images/recipe1.jpg" class="imgLeft" />
-<h3>Asia</h3>
+$content = '<img src="Images/maindish.jpg" class="imgLeft" />
+<h3>Main Dish</h3>
 <p>
-    Asian cuisine includes several major regional cuisines: East Asian, Southeast Asian, 
-South Asian, Central Asian, and Middle Eastern/Western Asian.  A cuisine is a characteristic 
-style of cooking practices and traditions usually associated with a specific culture. 
-Asia, being the largest and most populous continent, is home to many cultures, 
-many of which have their own characteristic cuisine.
+The main dish is usually the heaviest, heartiest, and most complex or substantial dish on a menu.
+The main ingredient is usually meat, fish or another protein source.
+The main dish is the main course of a meal.
+It is most often preceded by an appetizer, soup or salad, and followed by a dessert.
 </p>
 
-<img src="Images/recipe2.jpg" class="imgRight"/>
-<h3>Europe</h3>
+<img src="Images/appetizer.jpg" class="imgRight"/>
+<h3>Appetizer</h3>
 <p>
-    European cuisine, or alternatively Western cuisine, is a generalised term collectively referring 
-to the cuisines of Europe and other Western countries, including (depending on the definition) that of Russia, 
-as well as non-indigenous cuisines of Australasia, the Americas, Southern Africa, and Oceania, 
-which derive substantial influence from European settlers in those regions. 
-The term is used by East Asians to contrast with Asian styles of cooking. 
-When used by Westerners, the term may sometimes refer more specifically to cuisine in Europe; 
-in this context, a synonym is Continental cuisine, especially in British English.
+A hors d oeuvre, appetizer or starter is a small dish served before a meal. Some hors d oeuvres are served cold, others hot. Hors d oeuvres may be served at the dinner table as a part of the meal, or they may be served before seating.
+Some types may be refrigerated or frozen and then precooked and then reheated in an oven or microwave oven as necessary before serving.
 </p>
 
-<img src="Images/recipe3.jpg" class="imgLeft" />
-<h3>Canada</h3>
+<img src="Images/dessert.jpg" class="imgLeft" />
+<h3>Dessert</h3>
 <p>
-    Canadian cuisine varies widely depending on the regions of the nation. 
-The three earliest cuisines of Canada have First Nations, English, Scottish and French roots, 
-with the traditional cuisine of English Canada closely related to British cuisine, 
-while the traditional cuisine of French Canada has evolved from French cuisine and 
-the winter provisions of fur traders. With subsequent waves of immigration in the 19th and 20th century from Central, 
-Southern, and Eastern Europe, South Asia, East Asia, and the Caribbean, the regional cuisines were subsequently augmented.
+Dessert is a confectionery course that concludes a main meal.
+The course usually consists of sweet foods, and possibly a beverage such as dessert wine or liqueur, but may include coffee, cheeses, nuts, or other savory items.
+The term "dessert" can apply to many confections, such as cakes, tarts, cookies, biscuits, gelatins, pastries, ice creams, pies, puddings, custards, and sweet soups.
+Fruit is also commonly found in dessert courses because of its naturally occurring sweetness.
+Some cultures sweeten foods that are more commonly savory to create desserts.
 </p>';
 
 include 'Templates.php';
 ?>
-
