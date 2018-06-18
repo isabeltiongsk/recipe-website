f76531b7c33c1c01cffb005b2cefc35035f192cc
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-06-18 13:09:41 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php
+$title = "About";
+$content = '
+<h3>Summary</h3>
+<p>
+Foodee web helps you find your favorite food recipe and you can learn how to cook it.
+</p>
+
+<h3>Customer Service</h3>
+<p>
+To reach Recipe-Website Customer Support: </p>
+<p>
+Email:  isabeltiongsk@gmail.com
+Phone:  1-866-528-7784
+</p>
+<p>
+Email:  myatsutin@gmail.com
+Phone:  1-800-837-9017</p>
+
+<h3>Features</h3>
+<ul>
+  <li>Login system</li>
+  <li>Create cooking recipe</li>
+  <li>Read cooking recipe</li>
+  <li>Update cooking recipe</li>
+  <li>Delete cooking recipe</li>
+  <li>Use Google Map to find supermarket for ingredients</li>
+</ul>
+
+<h3>Version 6.0</h3>';
+
+include 'Templates.php';
+?>
