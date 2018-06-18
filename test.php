6d6683f0db1fc478cf11aa649f83c2185b39f6b2
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-06-18 13:11:12 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="banner">             
+            </div>
+            
+            <nav id="navigation">
+                <ul id="nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="Recipe.php">Recipe</a></li>
+                    <li><a href="test.php">Groceries</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="registration/login.php">Login</a></li>
+                </ul>
+            </nav>
+            
+            <div id="content_area">
+                <iframe src="MapProject/index.html" width="100%" height="500" ></iframe>
+            </div>
+            
+            <div id="sidebar">
+                
+            </div>
+             
+            <footer>
+                <p>All rights reserved</p>
+            </footer>
+        </div>
+    </body>
+</html>
